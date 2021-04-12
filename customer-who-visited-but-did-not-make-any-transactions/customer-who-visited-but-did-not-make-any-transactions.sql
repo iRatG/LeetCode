8e79ca8c528cdd56c3cebce009eb088781a38509
Ayrat Gilyazov <aratenator@gmail.com>
2021-04-12 15:59:58 +0300

    Time: 1252 ms (90.91%), Space: 0B (100.00%) - LeetHub

---
+++ customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+select customer_id,
+    count(*) count_no_trans
+    from visits
+        where visit_id not in 
+            (select visit_id from transactions)
+group by customer_id
