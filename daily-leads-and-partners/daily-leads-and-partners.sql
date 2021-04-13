0a1fe1755b55490edec01355c78382720c9b6c56
Ayrat Gilyazov <aratenator@gmail.com>
2021-04-13 18:12:40 +0300

    Time: 777 ms (39.51%), Space: 0B (100.00%) - LeetHub

---
+++ daily-leads-and-partners/daily-leads-and-partners.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+select 
+to_char(date_id,'yyyy-mm-dd') date_id, 
+make_name,
+count(distinct lead_id) unique_leads,
+count(distinct partner_id) unique_partners
+
+from DailySales
+group by date_id , make_name
