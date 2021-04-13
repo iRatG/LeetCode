c0fc2c39f553a16dcf6b275d3ced86ca5b8cb84e
Ayrat Gilyazov <aratenator@gmail.com>
2021-04-13 20:48:47 +0300

    Time: 675 ms (87.06%), Space: 0B (100.00%) - LeetHub

---
+++ products-price-for-each-store/products-price-for-each-store.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+select product_id,
+
+sum(case     when store='store1' then price else null end ) store1,
+sum(case     when store='store2' then price else null end ) store2,
+sum(case     when store='store3' then price else null end ) store3
+
+from products
+group by product_id
