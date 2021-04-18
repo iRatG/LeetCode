fa6be5a1aa2d48c6da19a3e57952da1d79674dbe
Ayrat Gilyazov <aratenator@gmail.com>
2021-04-18 23:48:15 +0300

    Time: 723 ms (91.30%), Space: 0B (100.00%) - LeetHub

--- unique-orders-and-customers-per-month/unique-orders-and-customers-per-month.sql
+++ unique-orders-and-customers-per-month/unique-orders-and-customers-per-month.sql
@@ -1,4 +1,5 @@
 /* Write your PL/SQL query statement below */
+/*
 select  month, 
         sum(case when order_count is null then 0 else order_count end) order_count,
         count(distinct customer_id) customer_count
@@ -9,8 +10,19 @@ from (
         ,customer_id
         ,count(distinct order_id) order_count
         ,sum(invoice) sumi
-    from orders where invoice > 20
+    from orders 
+    --where invoice > 20
 group by to_char(order_date,'yyyy-mm'), customer_id
 )
 where sumi > 20
 group by month
+*/
+
+select 
+        to_char(order_date,'yyyy-mm') month
+        ,count(distinct order_id) order_count
+        ,count(distinct customer_id) customer_count
+        
+    from orders 
+    where invoice > 20
+group by to_char(order_date,'yyyy-mm')
