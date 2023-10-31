a9de2b0bc020ae96007520bc31a4b00981985d8f
Ayrat Gilyazov <aratenator@gmail.com>
2023-10-31 20:56:52 +0300

    Time: 49 ms (28.94%) | Memory: 13.5 MB (10.50%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+   
+        if x < 0:
+            return False
+        else:
+            return str(x) == str(x)[::-1]   
